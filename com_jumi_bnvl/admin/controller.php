dbf1be9ce226d707a21c65a819db87a6ad4d35dc
Miguel Vázquez <miguel.vazquez@bonaval.com>
2024-08-06 13:55:18 +0200

    solve bug in backend with display params

--- com_jumi_bnvl/admin/controller.php
+++ com_jumi_bnvl/admin/controller.php
@@ -37,7 +37,20 @@ if(JV == 'j2') {
 else {
 	//j3 stuff here///////////////////////////////////////////////////////////////////////////////////////////////////////
 	class JumiController extends JControllerLegacy{
-		function display()
+		/**
+		 * Typical view method for MVC based architecture
+		 *
+		 * This function is provide as a default implementation, in most cases
+		 * you will need to override it in your own controllers.
+		 *
+		 * @param   boolean  $cachable   If true, the view output will be cached
+		 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+		 *
+		 * @return  \JControllerLegacy  A \JControllerLegacy object to support chaining.
+		 *
+		 * @since   3.0
+		 */
+		public function display($cachable = false, $urlparams = array()) // 
 		{
 			 
 			addSub( 'Application Manager', 'showapplications');
